ab05dfca57eb8b6337c5a012faf374539701171a
ryan.manny <pie4prez@gmail.com>
2019-10-09 15:39:09 -0700

    Move .env load into wsgi.py

--- mannyfesto/wsgi.py
+++ mannyfesto/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+project_folder = os.path.expanduser('~/mannyfesto')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mannyfesto.settings')
 
