08d9dc26da8e7280d4c032aa5b155fba015f30e1
ryan.manny <pie4prez@gmail.com>
2019-10-23 17:51:03 -0700

    Oops

--- core/signals.py
+++ core/signals.py
@@ -3,11 +3,11 @@ from django.db.models.signals import pre_save
 from core import models
 
 
-def post_post_save(sender, instance, *args, **kwargs):
+def post_pre_save(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = instance._get_unique_slug()
     instance.preview = instance._get_preview()  # Always recalculate preview
 
 
-pre_save.connect(post_post_save, sender=models.Post)
-pre_save.connect(post_post_save, sender=models.HTMLPost)
+pre_save.connect(post_pre_save, sender=models.Post)
+pre_save.connect(post_pre_save, sender=models.HTMLPost)
